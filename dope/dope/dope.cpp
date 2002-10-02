fbdf14b64432703eed08ddaf3898e22a89ce3f89
karme <karme>
2002-10-02 15:11:32 +0000

    try to print the stack backtrace on fatal errors (currently only on glibc systems)

--- dope/dope/dope.cpp
+++ dope/dope/dope.cpp
@@ -3,6 +3,38 @@
 #include <dopeconfig.h>
 #endif
 
+#ifdef HAVE_EXECINFO_H
+
+#include <execinfo.h>
+#include <stdlib.h>
+
+void 
+dope_backtrace()
+{
+#define MAXSTACK 200
+  void *array[MAXSTACK];
+  size_t size;
+  char **strings;
+  size_t i;
+  
+  size = backtrace (array, MAXSTACK);
+  strings = backtrace_symbols (array, size);
+  
+  std::cerr << "Obtained "<<size<<" stack frames.\n";
+  
+  for (i = 0; i < size; i++)
+    std::cout << strings[i] << std::endl;
+  
+  free (strings);
+}
+#else
+void 
+dope_backtrace()
+{
+}
+#endif
+
+
 int dope_micro_version=DOPE_MICRO_VERSION;
 int dope_minor_version=DOPE_MINOR_VERSION;
 int dope_major_version=DOPE_MAJOR_VERSION;
