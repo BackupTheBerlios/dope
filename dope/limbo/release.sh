48fd900a5316beb8a4041606bc5fc506fdc5e9b3
karme <karme>
2003-05-07 09:34:50 +0000

    cross compile

--- dope/limbo/release.sh
+++ dope/limbo/release.sh
@@ -16,48 +16,51 @@ cd $BUILDDIR
 
 #check that $PACKAGE directory does not already exist
 if test -e $PACKAGE; then
-	echo Warning $PACKAGE directory already exists - abort or I will remove it
-	read
-	rm -rf $PACKAGE
+    echo Warning $PACKAGE directory already exists - assuming we already have a distribution tarball in there
+    read
+else
+    #get source
+    cvs -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE login
+    cvs -z3 -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE co $PACKAGE
+
+    #build distribution tarball
+    cd $PACKAGE
+    ./bootstrap.sh
+    ./configure
+    make distcheck
 fi
 
-#get source
-cvs -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE login
-cvs -z3 -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE co $PACKAGE
-
-#build distribution tarball
-cd $PACKAGE
-./bootstrap.sh
-./configure
-make distcheck
+cd $BUILDDIR/$PACKAGE
 DISTFILE=`ls -1 $PACKAGE-*.tar.gz`
 DIR=${DISTFILE%.tar.gz}
 if [ "x$DIR" = "x" ]; then
     echo Fatal error
     exit 1
 fi
-cd $BUILDDIR
 
+cd $BUILDDIR
 if test -e $DIR; then
-	echo Warning $DIR directory already exists - abort or I will remove it
-	read
-	rm -rf $DIR
+    echo Warning $DIR directory already exists - assuming we already unpacked distribution
+    read
+else
+    tar xzvf $PACKAGE/$DISTFILE
 fi
-tar xzvf $PACKAGE/$DISTFILE
 
 #build in seperate directory
-mkdir -f build
+mkdir -p build
 cd build
 
 #native arch
-mkdir -f arch-native
+mkdir -p arch-native
 cd arch-native
 if test -e $DIR; then
 	echo Warning $DIR directory already exists - abort or I will remove it
 	read
 	rm -rf $DIR
 fi
+mkdir $DIR
 cd $DIR
+
 $BUILDDIR/$DIR/configure --disable-shared --prefix=$PREFIX
 make install
 
@@ -65,13 +68,14 @@ make install
 if test -e $SETUPCROSS; then
     source $SETUPCROSS
     cd $BUILDDIR/build
-    mkdir -f arch-cross
+    mkdir -p arch-cross
     cd arch-cross
     if test -e $DIR; then
 	echo Warning $DIR directory already exists - abort or I will remove it
 	read
 	rm -rf $DIR
     fi
+    mkdir $DIR
     cd $DIR
     $BUILDDIR/$DIR/configure $CROSS_CONFIGURE_OPTIONS $DOPE_CROSS_CONFIGURE_OPTIONS --prefix=$PREFIX/cross
     make install
